1b8d379bb2f364b65373fd25df16a121ec668433
Daniel <235312011+fischblob-lol@users.noreply.github.com>
2026-07-22 18:18:48 +0200

    Add initial Sphinx configuration in conf.py

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+# proudly written in nano by firewald
+project = 'xpk'
+copyright = '2026, aurelius, firewald'
+author = 'aurelius, firewald'
+release = '21/7/2026'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# good contender
+# html_theme = 'sphinx_book_theme' 
+html_theme = 'furo' 
+html_static_path = ['_static']
